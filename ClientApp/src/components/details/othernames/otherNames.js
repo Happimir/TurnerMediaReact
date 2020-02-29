8df6368605ed6d483a33329337f0b585cd58b8c1
Michael Kovalsky <mkovalsky@datafinch.com>
2020-02-29 01:24:57 -0500

    minor change to some ui element

--- ClientApp/src/components/details/othernames/otherNames.js
+++ ClientApp/src/components/details/othernames/otherNames.js
@@ -2,10 +2,11 @@
 import { Item } from 'semantic-ui-react';
 
 export const OtherNames = (props) => {
+
     return (
         <Item.Content>
             <Item.Description>Language: {props.othername.titleNameLanguage}</Item.Description>
-            <Item.Description>Primary Language: {props.othername.titleNameType === "Primary"}</Item.Description>
+            <Item.Description>Primary Language: {props.othername.titleNameType === "Primary" ? "Yes" : "No"}</Item.Description>
             <Item.Description>Title Name: {props.othername.titleName}</Item.Description>
         </Item.Content>    
     )
--- ClientApp/src/components/details/storylines/storylines.js
+++ ClientApp/src/components/details/storylines/storylines.js
@@ -5,7 +5,7 @@ export const Storylines = (props) => {
     return (
         <Item.Content>
             <Item.Description>{props.story.description}</Item.Description>
-            <Item.Meta>Language: {props.story.language} } Type: {props.story.type}</Item.Meta>
+            <Item.Meta>Language: {props.story.language} | Type: {props.story.type}</Item.Meta>
         </Item.Content>
     )
 }
